175c71f65e4b676bc8259ac6c1b8faad4b6cb0f0
Aditya Wanjari <aditya@Adityas-MacBook-Pro.local>
2025-08-14 17:44:12 -0400

    added sample config file

---
+++ sample_config.py
@@ -0,0 +1,13 @@
+{
+  "mcpServers": {
+    "predictTrains": {
+      "command": "/Users/aditya/.local/bin/uv", # Full path to your uv folder
+      "args": [
+        "--directory",
+        "/Users/aditya/Desktop/Citadel/GenAI/Projects/MCP/mbta",
+        "run",
+        "server_predict_trains.py"
+      ]
+    }
+  }
+}
